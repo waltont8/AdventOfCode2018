045e5f1e30a283b8c7390ad3b062461aa7b28e54
waltont8 <2472981+waltont8@users.noreply.github.com>
2021-04-10 15:32:19 +0100

    Day 3

---
+++ README.md
@@ -0,0 +1,6 @@
+I didn't know about Advent of code in 2018 so going back to have a look at the puzzles
+
+--- Day 1: Chronal Calibration ---
+--- Day 2: Inventory Management System ---
+--- Day 3: No Matter How You Slice It ---
+    Playing around with attoparsec. Takes a few seconds to run, but would have taken a few minutes to write a faster one
---
+++ day_03/Lib.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE OverloadedStrings #-}   -- Add at the top of the file
+
+
+module Lib
+    ( someFunc
+    ) where
+
+import Data.Text as T (pack)
+import Data.List
+import Data.Either
+import Control.Arrow
+import Data.Attoparsec.Text as P
+
+data InputLine = InputLine Int Int Int Int Int deriving (Show)
+
+inputParser :: Parser InputLine
+inputParser = InputLine <$  char '#'
+                        <*> decimal
+                        <*  skipSpace
+                        <*  char '@'
+                        <*  skipSpace
+                        <*> decimal
+                        <*  char ','
+                        <*> decimal
+                        <*  char ':'
+                        <*  skipSpace
+                        <*> decimal
+                        <*  string "x"
+                        <*> decimal
+
+inputLineToList :: InputLine -> [(Int, Int)]
+inputLineToList (InputLine idx x y w h) = [ (a,b) | a <- [x..(x+w-1)], b <- [y..(y+h-1)]]
+
+
+someFunc :: IO ()
+someFunc = do
+        input <- (fmap lines $ readFile "input.txt")
+        let rects = map (fromRight (InputLine 0 0 0 0 0)) $ map ((parseOnly inputParser) . T.pack) input
+        let pointList = map inputLineToList rects
+        print $ (concat >>> sort >>> group >>> (map length) >>> (filter (>1)) >>> length) pointList
+        let db = (concat >>> sort) pointList
+        print $ findTheSquare rects db
+
+findTheSquare (h:xs) db 
+    | (any hitPoint thisRect) == True = findTheSquare xs db
+    | otherwise = h
+    where
+        thisRect = inputLineToList h
+        hitPoint (x,y) = if length (filter (==(x,y)) db) > 1 then True else False
+
